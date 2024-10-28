7a826bd7200d3a7c8330259fbd0fc3b09178aa4e
znogbes <juansegonmar@gmail.com>
2024-10-28 15:22:54 +0000

    Tested folder creation in restricted library

--- config_sharepoint_location.R
+++ config_sharepoint_location.R
@@ -1,8 +1,8 @@
 library(Microsoft365R)
-site_url <- "https://nhsengland.sharepoint.com/sites/MED/ps2/psi"
+site_url <- "https://nhsengland.sharepoint.com/sites/MED/ps2/it/mit"
 site <- get_sharepoint_site(site_url = site_url, tenant="nhsengland")
-chosenlib <- site$get_drive("Open Library")
-base_url <- "03 Programme Management/04-PSCs/02-Assurance/13 - QART automation demo/"
+chosenlib <- site$get_drive("Restricted Library")
+base_url <- "Measurement/QART"
 
 
 location_lookup <- read.csv("psc_lookup.csv")
