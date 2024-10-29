85042620b1562258a94d0ffc368343e7136218a3
znogbes <juansegonmar@gmail.com>
2024-10-29 16:56:13 +0000

    Completed first test of functionalised visualisation on slide deck

--- run_quarto.R
+++ run_quarto.R
@@ -1,8 +1,10 @@
 #run quarto fie and save output in output folder
 library(here)
 library(quarto)
+library(glue)
 
 quarto_render("qart_slides.qmd")
+file_name <- format(Sys.time(), "qart_test_%Y_%m_%d_%H%M%S.pptx")
 
 #move the quarto report into the output folder and change name
-file.rename('qart_slides.pptx', here('output/test.pptx'))
+file.rename('qart_slides.pptx', here(glue::glue('output/{file_name}')))
