12c7e7bd8e23a625129d7d3d1373ee51a4aaa630
znogbes <juansegonmar@gmail.com>
2024-10-31 18:12:02 +0000

    Tweaks in pptx template and functions have been tested

--- run_quarto.R
+++ run_quarto.R
@@ -3,7 +3,12 @@ library(here)
 library(quarto)
 library(glue)
 
-quarto_render("qart_slides.qmd")
+#### MAKE A QUARTER PARAMETER
+
+quarto_render("qart_slides.qmd"#, 
+              #execute_params = list('a_quarter' = a_quarter)
+              )
+
 file_name <- format(Sys.time(), "qart_test_%Y_%m_%d_%H%M%S.pptx")
 
 #move the quarto report into the output folder and change name
