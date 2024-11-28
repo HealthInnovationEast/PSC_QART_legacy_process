a820abb3cf3ba456e7345e415e2b71b1ddb8ac4f
znogbes <juansegonmar@gmail.com>
2024-11-28 17:24:10 +0000

    Apply suggestions from code review

--- config_sharepoint_location.R
+++ config_sharepoint_location.R
@@ -1,6 +1,6 @@
 library(Microsoft365R)
-site_url <- "https://nhsengland.sharepoint.com/sites/MED/ps2/it/mit"
-site <- get_sharepoint_site(site_url = site_url, tenant="nhsengland")
+site_url <- "https://nhs.sharepoint.com/sites/MED/ps2/it/mit"
+site <- get_sharepoint_site(site_url = site_url, tenant="nhs")
 chosenlib <- site$get_drive("Restricted Library")
 base_url <- "Measurement/QART"
 
