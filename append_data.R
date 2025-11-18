4cb263d437627ffaedf0505f11a13d8b59204be5
znogbes <juansegonmar@gmail.com>
2025-11-18 10:59:42 +0000

    set up loop procedure to append martha's and matneo data

--- append_data.R
+++ append_data.R
@@ -1,34 +1,79 @@
-# retrieve martha's rule submissions
-# place holder as the process of fetching previous data and appending new data is not yet in place
-# this will be ready for 2526 Q2
+previous_submissions <- c(previous_marthas_submissions_file_name,
+                          previous_mat_neo_submissions_file_name
+                          )
 
-# retrieve previous matneo submissions data
-previous_mat_neo_submissions_cleansed <- read.csv(here(str_glue('output/{previous_mat_neo_submissions_file_name}')))
+for (previous_submission in previous_submissions){
+  # download previous submission from sharepoint
+  previous_submission_file <- master_files |>
+    select(name) |>
+    # submissions must be saved with returned ending on file name
+    filter(str_detect(name, previous_submission))
+  
+  if (nrow(previous_submission_file) == 0) {
+    stop(glue::glue("file not found in location '{master_files_location}':\n file name provided: '{previous_submission}'"))
+  }
+  
+  previous_submission_file_item <- master_files_location$get_item(previous_submission_file)
 
-message('Previous MatNeo submissions have data up to ', max(previous_mat_neo_submissions_cleansed$quarter))
+  # download file 
+  previous_submission_path <- here('data', 
+                                   str_glue('{previous_submission_file}'))
+  
+  previous_submission_file_item$download(
+    dest = previous_submission_path,
+    overwrite = T
+  )
+  
+  # read previous submissions data
+  previous_submission_data <- read.csv(previous_submission_path)
+  
+  if ( str_detect(previous_submission, 'marthas') ) {
+    message("Previous Martha's submissions have data up to ", 
+            max(previous_submission_data$quarter))
+    
+    # collated data produced in read_quart_submissions.R
+    reporting_quarter_submissions <- read.csv(
+      here(marthas_submissions_path)) |>
+      clean_names() 
+    
+    # bind rows
+    all_submissions <- previous_submission_data |>
+      bind_rows(reporting_quarter_submissions) 
+    
+    message("Saving updated Martha's data to SharePoint")
+    
+    file_name <- glue('marthas_qart_all_data_upto_{quarter_string}.csv')
+    
+  } else if ( str_detect(previous_submission, 'mat_neo') ) {
+    
+    message("Previous MatNeo submissions have data up to ", 
+            max(previous_submission_data$quarter))
+    
+    # collated data produced in read_quart_submissions.R
+    reporting_quarter_submissions <- read.csv(
+      here(mat_neo_opt_submissions_path)) |>
+      clean_names() 
+    
+    # bind rows
+    all_submissions <- previous_submission_data |>
+      bind_rows(reporting_quarter_submissions) 
+    
+    message('Saving updated MatNeo data to SharePoint')
+    
+    file_name <- glue('mat_neo_qart_all_data_upto_{quarter_string}.csv')
+  }
+  
+  write.csv(all_submissions, 
+            str_glue("output/{file_name}"),
+            row.names = F)
+  
+  chosenlib$upload_file(
+    dest = str_glue("{base_url}/{master_files_folder}/{file_name}"),
+    src = str_glue("output/{file_name}")
+  )
+  
+  message('Submissions for ', unique(reporting_quarter_submissions$quarter), 
+          ' have been appended')
+  
+}
 
-# collated data from psc produced in read_quart_submissions.R
-reporting_quarter_mat_neo_submissions <- read.csv(here(mat_neo_opt_submissions_path)) |>
-  clean_names() 
-
-message('Submissions for ', unique(reporting_quarter_mat_neo_submissions$quarter), 
-        ' will be appended by this script')
-
-# bind rows
-all_submissions <- previous_mat_neo_submissions_cleansed |>
-  bind_rows(reporting_quarter_mat_neo_submissions) 
-
-# write to share point
-message("Saving updated Martha's Rule data to SharePoint")
-
-
-message('Saving updated MatNeo data to SharePoint')
-
-write.csv(all_submissions, 
-          str_glue("output/mat_neo_qart_all_data_upto_{quarter_string}.csv"),
-          row.names = F)
-
-chosenlib$upload_file(
-  dest = str_glue("{base_url}/{master_files_folder}/mat_neo_qart_all_data_upto_{quarter_string}.csv"),
-  src = str_glue("output/mat_neo_qart_all_data_upto_{quarter_string}.csv")
-)
