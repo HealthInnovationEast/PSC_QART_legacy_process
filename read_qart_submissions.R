a6d5b18b015f9e5dd56317a1eb0c3a6628b4a8e6
znogbes <juansegonmar@gmail.com>
2026-08-19 22:02:00 +0100

    adjusteed function to read MR ed data correctly

--- read_qart_submissions.R
+++ read_qart_submissions.R
@@ -30,7 +30,9 @@ write.csv(hin_names,
 #' @param sheet_range String. Valid excel sheet range where data is recorded (e.g., "B6:M30")
 #' @param del_cols Vector. Column names to be deleted. These columns do not contain data. 
 #'                 They serve layout purposes in excel sheet 
-#' @param col_names Vector. Tidy names to be used to rename columns 
+#' @param tidy_col_names Vector. Tidy names to be used to rename columns 
+#' @param col_names Boolean. Controls whether first row of range provided is used as col names
+#'                  Defaulted to TRUE
 #' @param hin_name String. Name of a health innovation network that submitted data
 
 #' @return Tidy table containing adoption data
@@ -40,14 +42,16 @@ collect_sheet_data <- function(
     sheet_name, 
     sheet_range, 
     del_cols, 
-    col_names,
+    tidy_col_names,
+    col_names = T,
     hin_name){
   
   # locate raw data submiited by HIN
   data_marthas <- read_excel(
     path = tf, 
     sheet = sheet_name,
-    range = sheet_range 
+    range = sheet_range,
+    col_names = col_names
   ) |> 
     clean_names() |>
     # no data in this column, used for better print out in excel doc
@@ -55,10 +59,11 @@ collect_sheet_data <- function(
     remove_empty("rows")
   
   # assign tidy names
-  names(data_marthas) <- col_names
+  names(data_marthas) <- tidy_col_names
   
   data_marthas_tidy <- data_marthas |>
-    filter(if_any(matches('adults|paediatric'), ~!is.na(.))) |>
+    # TODO: REMOVE ???
+    #filter(if_any(matches('adults|paediatric'), ~!is.na(.))) |>
     mutate(updated_psc_name = hin, .before = trust_code) |>
     mutate(
       quarter = reporting_quarter_string,
@@ -83,10 +88,13 @@ collect_sheet_data <- function(
 
 # empty data frames to save submissions
 results_marthas_adult_paeds <- tibble()
+results_marthas_mat_neo <- tibble()
+results_marthas_ed <- tibble()
+
 #results_mat_neo <- tibble()
 
 # for testing 
-#hin_folders = 'Manchester HIN'
+# hin_folders = 'Manchester HIN'
 
 for (hin in hin_folders) {
   # identify submission
@@ -130,7 +138,7 @@ for (hin in hin_folders) {
   # read martha's rule data
   message("Reading Martha's Rule submissions: adult and paeds")
   
-  col_names <- c('trust_code', 'name_of_trust', 
+  col_names_adult_paeds <- c('trust_code', 'name_of_trust', 
                  'site_code', 'name_of_site', 'phase',
                  'adults_patient_check_in', 
                  'adults_independent_clinical_review', 
@@ -139,17 +147,89 @@ for (hin in hin_folders) {
                  'paediatric_independent_clinical_review', 
                  'paediatric_escalation_available_to_patient_carers')
   
-  # deploy function
-  data_marthas_tidy <- collect_sheet_data(
+  # execute function
+  data_marthas_tidy_adult_paeds <- collect_sheet_data(
     tf = tf, 
     sheet_name = "MR - Adult & Paeds", 
     sheet_range = "B6:M30", 
     del_cols = c('x9'), 
-    col_names = col_names,
+    tidy_col_names = col_names_adult_paeds,
+    hin_name = hin)
+  
+  # repeat for matneo settings
+  message("Reading Martha's Rule submissions: maternity and neonatal")
+  
+  col_names_adult_paeds <- c('trust_code', 'name_of_trust', 
+                             'site_code', 'name_of_site',
+                             'antenatal_patient_check_in', 
+                             'antenatal_independent_clinical_review', 
+                             'antenatal_escalation_available_to_patient_carers',
+                             'intrapartum_patient_check_in', 
+                             'intrapartum_independent_clinical_review', 
+                             'intrapartum_escalation_available_to_patient_carers',
+                             'postnatal_patient_check_in', 
+                             'postnatal_independent_clinical_review', 
+                             'postnatal_escalation_available_to_patient_carers',
+                             'transitional_patient_check_in', 
+                             'transitional_independent_clinical_review', 
+                             'transitional_escalation_available_to_patient_carers',
+                             'neonatal_patient_check_in', 
+                             'neonatal_independent_clinical_review', 
+                             'neonatal_escalation_available_to_patient_carers'
+                             )
+  
+  data_marthas_tidy_mat_neo <- collect_sheet_data(
+    tf = tf, 
+    sheet_name = "MR - Maternity & Neonatal", 
+    sheet_range = "B6:X30", 
+    del_cols = c('x8', 'x12', 'x16', 'x20'), 
+    tidy_col_names = col_names_adult_paeds,
+    hin_name = hin)
+  
+  # and for ed 
+  message("Reading Martha's Rule submissions: emergency department")
+  
+  col_names_ed <- c('trust_code', 'name_of_trust', 
+                     'site_code', 'name_of_site',
+                     'adults_waiting_room_patient_check_in', 
+                     'adults_waiting_room_independent_clinical_review', 
+                     'adults_waiting_room_escalation_available_to_patient_carers',
+                     'adults_majors_patient_check_in', 
+                     'adults_majors_independent_clinical_review', 
+                     'adults_majors_escalation_available_to_patient_carers',
+                     'adults_tes_patient_check_in', 
+                     'adults_tes_independent_clinical_review', 
+                     'adults_tes_escalation_available_to_patient_carers',
+                     'adults_resus_patient_check_in', 
+                     'adults_resus_independent_clinical_review', 
+                     'adults_resus_escalation_available_to_patient_carers',
+                     'paediatric_waiting_room_patient_check_in', 
+                     'paediatric_waiting_room_independent_clinical_review', 
+                     'paediatric_waiting_room_escalation_available_to_patient_carers',
+                     'paediatric_majors_patient_check_in', 
+                     'paediatric_majors_independent_clinical_review', 
+                     'paediatric_majors_escalation_available_to_patient_carers',
+                     'paediatric_resus_patient_check_in', 
+                     'paediatric_resus_independent_clinical_review', 
+                     'paediatric_resus_escalation_available_to_patient_carers'
+  )
+  
+  data_marthas_tidy_ed <- collect_sheet_data(
+    tf = tf, 
+    sheet_name = "MR - Emergency Departments", 
+    sheet_range = "B7:AF30", 
+    del_cols = c('x8', 'x12', 'x16', 'x20', 'x24', 'x28'), 
+    tidy_col_names = col_names_ed,
+    col_names = FALSE,
     hin_name = hin)
   
   # append extracted data to list
-  results_marthas_adult_paeds <- bind_rows(results_marthas_adult_paeds, data_marthas_tidy)
+  results_marthas_adult_paeds <- bind_rows(results_marthas_adult_paeds, 
+                                           data_marthas_tidy_adult_paeds)
+  results_marthas_mat_neo <- bind_rows(results_marthas_mat_neo, 
+                                       data_marthas_tidy_mat_neo)
+  results_marthas_ed <- bind_rows(results_marthas_ed, 
+                                  data_marthas_tidy_ed)
   
   # # read matneo data
   # message("Reading MatNeo submissions")
@@ -257,7 +337,9 @@ for (hin in hin_folders) {
   
 }
 
-if (length(unique(results_marthas_adult_paeds$updated_psc_name)) != 15 #& 
+if (length(unique(results_marthas_adult_paeds$updated_psc_name)) != 15 & 
+    length(unique(results_marthas_mat_neo$updated_psc_name)) != 15 &
+    length(unique(results_marthas_ed$updated_psc_name)) != 15 
     #length(unique(results_mat_neo$updated_psc_name)) != 15
     ){
   stop('Data not appended correctly')
@@ -268,13 +350,29 @@ quarter_string <- reporting_quarter_string |>
   str_replace_all("/| ", "_")
 
 time_stamp_ext <- format(Sys.time(), "%Y_%m_%d_%H%M%S.csv")
-marthas_submissions_path <- glue::glue("output/marthas_psc_submissions_{quarter_string}_processed_{time_stamp_ext}")
+
+# create paths for saving 
+marthas_adults_paeds_submissions_path <- glue::glue("output/marthas_adults_paeds_psc_submissions_{quarter_string}_processed_{time_stamp_ext}")
+marthas_mat_neo_submissions_path <- glue::glue("output/marthas_mat_neo_psc_submissions_{quarter_string}_processed_{time_stamp_ext}")
+marthas_ed_submissions_path <- glue::glue("output/marthas_ed_psc_submissions_{quarter_string}_processed_{time_stamp_ext}")
 #mat_neo_opt_submissions_path <- glue::glue("output/mat_neo_optimisation_psc_submissions_{quarter_string}_processed_{time_stamp_ext}")
 
-write.csv(results_marthas_adult_paeds,
-  file = here(marthas_submissions_path),
-  row.names = F
-)
+# loop over results and file names to save as csv's
+results <- list(results_marthas_adult_paeds,
+           results_marthas_mat_neo,
+           results_marthas_ed)
+
+paths <- c(marthas_adults_paeds_submissions_path,
+           marthas_mat_neo_submissions_path,
+           marthas_ed_submissions_path)
+
+for (i in seq_along(results)){
+  write.csv(
+    x = results[[i]],
+    file = here(paths[[i]]),
+    row.names = F
+  )
+}
 
 # write.csv(results_mat_neo,
 #           file = here(mat_neo_opt_submissions_path),
